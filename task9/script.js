ccb79f3cc610a7701ca0c7f3c14bc764f1254990
hauntedbae <rima.sakalauskaite4121@gmail.com>
2022-07-13 21:59:31 +0300

    Add js with constructor class

---
+++ task9/script.js
@@ -0,0 +1,30 @@
+/* ------------------------------ TASK 9 ---------------------------------------------------------------
+Sukurkite konstruktoriaus funkciją "Movie" (naudokte ES6), kuri gebės sukurti objektus 3 savybėm ir 1 metodu.
+
+Savybės:
+title: string
+director: string
+budget: number
+
+Metodas: 
+wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
+------------------------------------------------------------------------------------------------------ */
+class Movie {
+  constructor(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+  }
+
+  wasExpensive = () => {
+    return this.budget >= 100000000;
+  };
+}
+
+const filmBudget = new Movie(
+  "Blade Runner 2049",
+  "Denis Villeneuve",
+  150000000
+);
+
+console.log(filmBudget.wasExpensive());
